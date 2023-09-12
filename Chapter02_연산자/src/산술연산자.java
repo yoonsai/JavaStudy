0b647c4f4f3706ed4cdf191e47ab01d850f7726a
SIST <SIST@DESKTOP-V4HDVS5>
2023-09-12 17:49:42 +0900

    5555

---
+++ Chapter02_연산자/src/산술연산자.java
@@ -0,0 +1,28 @@
+
+public class 산술연산자 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int kor=89;
+		int eng=90;
+		int math=87;
+		int total=kor+eng+math;
+		double avg=total/3.0;
+		
+		System.out.println("평균 :"+avg);
+		System.out.printf("평균 :%.2f",avg);
+		System.out.println("=======");
+		
+		int a=5;
+		int b=2;
+		System.out.println(a+"%"+b+"="+(5%2));
+		System.out.println(a+"%"+b+"="+(-5%2));
+		System.out.println(a+"%"+b+"="+(5%-2));
+		System.out.println(a+"%"+b+"="+(-5%-2));
+		System.out.println("==========형변환==========");
+		/*
+		 * 
+		 */
+	}
+
+}
