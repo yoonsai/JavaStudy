8f8456de2a87b22da63621c42ec4d53401964628
SIST <SIST@DESKTOP-V4HDVS5>
2023-10-13 09:03:51 +0900

    ddd

---
+++ Chapter05_접근지정어/src/com/sist/main/MainClass.java
@@ -0,0 +1,11 @@
+package com.sist.main;
+import com.sist.util.User; // 다른 패키지(폴더)에 있으면 import를 이용해서 사용할 수 있음 / 같은 패키지는 가능
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MainClass2 m=new MainClass2();
+		User u=new User();
+	}
+
+}
---
+++ Chapter05_접근지정어/src/com/sist/util/User.java
@@ -0,0 +1,10 @@
+package com.sist.util;
+
+public class User {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
