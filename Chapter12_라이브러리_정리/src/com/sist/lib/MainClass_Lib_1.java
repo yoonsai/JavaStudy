fe1802e7aae2d2f11ce68c8ce2da86d0015e353d
yoonsai <micelle0716@naver.com>
2024-01-25 17:21:22 +0900

    라이브러리 정리

---
+++ Chapter12_라이브러리_정리/src/com/sist/lib/MainClass_Lib_1.java
@@ -0,0 +1,78 @@
+package com.sist.lib;
+/*
+ *  라이브러리
+ *    **= java.lang
+ *        Object
+ *        String
+ *        StringBuffer
+ *        Math
+ *        Wrapper
+ *        -----------------
+ *    **= java. util
+ *        StringTokenizer
+ *        Date / Calendar
+ *        ArrayList
+ *        HashSet
+ *        HashMap
+ *        
+ *    **= java.io
+ *        FileInputStream / FileOutputStream
+ *        FileReader
+ *        BufferedReader
+ *        BufferedInputStream / BufferedOutputStream
+ *        ------------------------------------------------
+ *    = java.sql : Connection / Statement / ResultSet
+ *    = java.text, java.net : URL / URLEncoder / Socket
+ *    ------------- 자바를 사용하는 모든 소스의 기본
+ *    = java.http.servlet (Web) ==> JSP
+ *    = 외부 라이브러리
+ *      org.springframework....
+ *      org.mybatis...
+ *      org.jsoup...
+ *      
+ *    = 사용자 정의
+ *      => 기본
+ *         변수 
+ *         => 데이터형 
+ *            정수 : int, long, byte
+ *            실수 : double
+ *            논리 : boolean
+ *            문자열 : String
+ *           
+ *            연산자 
+ *             => 증감연산자 (++,--)
+ *             => 부정연산자 (!)
+ *             => 형변환연산자 (type)
+ *             => 산술연산자 (+,=,*,/,%)
+ *             => 비교연산자 (<,>,<=,>=,==,!=)
+ *             => 대입(=,+=,-=)
+ *            
+ *            제어문 
+ *             => if
+ *             => if~else
+ *             => for => for~each
+ *             => while
+ *             => break
+ *             
+ *      => 클래스
+ *         = 멤버변수 / 공유변수
+ *         = 메소드
+ *         = 생성자
+ *           => 초기화 / 시작과 동시에 처리
+ *           => 리턴형이 없다
+ *      => 특성
+ *         = 캡슐화 / 상속/포함 / 오버라이딩
+ *      => 제어어
+ *         static / abstract / final
+ *      => 접근지정어
+ *         private / default / public
+ *     
+ */
+public class MainClass_Lib_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
